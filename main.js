560f6bd7d60a9a19c6cd875d29dc61eb6a5006af
unknown <hadeel.f.hazimeh@gmail.com>
2023-11-13 12:34:45 +0300

    losing the game

--- main.js
+++ main.js
@@ -17,6 +17,14 @@ const images = [
   { id: 2, src: "./images/img 2.jpg" },
 ];
 
+
+
+
+
+let clickedImage = [];
+let background=[]
+let wrongiteration=0;
+
 /*Render the images 
 //I want to render the images with the 
 shape of layout 3rows*4columns 
@@ -24,8 +32,6 @@ then waiting for 10 sec
 then rotate them*
 the fucnction will iterate on the images array: creating div for each images
 */
-
-let clickedImage = [];
 const renderImages = () => {
   let click = 0;
   images.forEach((images, i) => {
@@ -36,8 +42,8 @@ const renderImages = () => {
     divBackground.classList.add("background");
     const image = document.createElement("img"); //create image element
     image.src = images.src; //access the src from the object
-    image.id = `img-card${images.id}`;
-    divBackground.id = `background${images.id}`;
+    image.id = `img-card${i}`;
+    divBackground.id = `background${i}`;
 
     image.classList.add("img-card"); //id for the image
     imageBox.append(image, divBackground); // adding the image inside the div imageBox
@@ -48,53 +54,81 @@ const renderImages = () => {
       image.style.display = "block";
       // console.log(images);
       clickedImage.push(images.id);
-      check(); //e,a
+      background.push(i);
+      check(i); //e,a
     });
     
   });
 };
 //console.log(clickedImage);
 renderImages();
+//box for the paragraph and button
+const tryAgainBox =document.createElement("div");
+tryAgainBox.id="tryAgain-box"
+// creating paragraph after losing 
+const youLose=document.createElement("p");
+youLose.id="you-lose"
+youLose.innerText=" You LOSE! Please try again"
+// create button try again 
 
-const check = () => {
-  // console.log(e.target)
-  if (clickedImage.length === 2) {
-    // document.querySelectorAll(#img-card)
+const tryAgain=document.createElement("button");
+const body=document.createElement("body");
+tryAgain.id="try-again";
+tryAgain.innerText=" Try Again";
+
+tryAgainBox.append(youLose,tryAgain);
+main.append(tryAgainBox);
+
+const check = (i) => {
+   if (clickedImage.length === 2) {
+    //sound true
     console.log(clickedImage);
     if (clickedImage[0] === clickedImage[1]) {
       //check the id of clicked images
       console.log(true);
-      clickedImage.splice(0, clickedImage.length); //delete the array elements
+      clickedImage.splice(0, clickedImage.length);
+       //delete the array elements
+       background.splice(0,background.length);
       console.log(clickedImage.length);
     } else {
       console.log(false);
-      // console.log(clickedImage[0], clickedImage[1]);
+       console.log(background[0], background[1]);
       //query select for the image and background (have the same id)
-      const img1 = document.querySelector(`#img-card${clickedImage[0]}`);
-  console.log(img1)
+      const img1 = document.querySelector(`#img-card${background[0]}`);
+  
       const background1 = document.querySelector(
-        `#background${clickedImage[0]}`
+        `#background${background[0]}`
       );
 
-      const img2 = document.querySelector(`#img-card${clickedImage[1]}`);
+      const img2 = document.querySelector(`#img-card${background[1]}`);
       console.log(img2)
       const background2 = document.querySelector(
-        `#background${clickedImage[1]}`
+        `#background${background[1]}`
       );
-      console.log(background1,background2)
+      console.log(background)
+      
       // img1.style.pointerEvents = "none";
       
       setTimeout(()=>{
         img1.style.display = "none";
         background1.style.display = "block";
-      
         img2.style.display = "none";
         background2.style.display = "block"; 
-      },"1000")
+      },3000)
           
       // console.log(clickedImage);
       clickedImage.splice(0, clickedImage.length);
-      
+      background.splice(0,background.length);
+       wrongiteration+=1
+       //sound wrong
+       console.log(wrongiteration)
+       //if 3 wrong iterations => lose 
+   
+       if (wrongiteration===3)
+       {
+        youLose.style.display= "inline"
+        
+       }
     }
   }
 };
